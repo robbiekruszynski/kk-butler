1f9c98cc6b897a398a6fc9db9cb54f783841e2d3
robbie <robbiekruszynski@gmail.com>
2019-12-27 17:51:39 -0800

    fix scaffolding in data structure

--- src/Card.js
+++ src/Card.js
@@ -2,36 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Card = ({ property }) => {
-  const {
-    index,
-    picture,
-    city,
-    address,
-    bedrooms,
-    bathrooms,
-    carSpaces
-  } = property;
+  const { index, picture, brand, material } = property;
   return (
     <div id={`card-${index}`} className="card">
-      <img src={picture} alt={city} />
+      <img src={picture} alt={brand} />
       <div className="details">
         <span className="index">{index + 1}</span>
         <p className="location">
-          {city}
+          brand: {brand}
           <br />
-          {address}
+          material: {material}
         </p>
-        <ul className="features">
-          <li className="icon-bed">
-            {bedrooms} <span>bedrooms</span>
-          </li>
-          <li className="icon-bath">
-            {bathrooms} <span>bathrooms</span>
-          </li>
-          <li className="icon-car">
-            {carSpaces} <span>parking spots</span>
-          </li>
-        </ul>
       </div>
     </div>
   );
--- src/data/data.js
+++ src/data/data.js
@@ -5,8 +5,8 @@ const data = {
       index: 0,
       picture:
         "https://s7ondemand5.scene7.com/is/image/EileenFisher/F9AOR-W5011M-270-ALT2?$RETINA_X_174$",
-      Brand: "Eileen Fisher",
-      Material: "wool"
+      brand: "Eileen Fisher",
+      material: "wool"
     },
     {
       _id: "593e9297ec4cca9c56bf61af",
@@ -14,8 +14,8 @@ const data = {
       price: 703725,
       picture:
         "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property02.jpg",
-      Brand: "Zara",
-      Material: "cotton"
+      brand: "Zara",
+      material: "cotton"
     },
     {
       _id: "593e929787f567dccda86910",
@@ -23,8 +23,8 @@ const data = {
       price: 707939,
       picture:
         "https://ihatetomatoes.net/demos/_rw/01-real-estate/tn_property05.jpg",
-      Brand: "Gucci",
-      Material: "silk"
+      brand: "Gucci",
+      material: "silk"
     }
   ]
 };
